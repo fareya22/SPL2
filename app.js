cff52bfb4665a0b28189fa6e8ac05de5943daf1b
Fareya Azam <113286085+fareya22@users.noreply.github.com>
2024-02-09 10:01:17 +0600

    Add files via upload

--- app.js
+++ app.js
@@ -1,22 +1,108 @@
 const express = require('express');
+const { default: mongoose } = require('mongoose');
+const morgan = require('morgan');
+const Blog =  require('./models/blog');
+const cors = require('cors');
 
 const app = express();
+app.use(cors());
+app.use(express.json());
 
-app.listen(3000);
-app.get('/',(req,res) => {
-    
-    res.sendFile('./views/index.html',{root: __dirname});
-});
+const dbURl = 'mongodb+srv://bsse1331:mongodbFAREYA22@cluster0.hodzqty.mongodb.net/node-tuts?retryWrites=true&w=majority';
+mongoose.connect(dbURl, {useNewUrlParser: true, useUnifiedTopology: true})
+     .then((result) => app.listen(3000))
+     .catch((err) => console.log(err));
 
-app.get('/about',(req,res) => {
-    
-    res.sendFile('./views/about.html',{root: __dirname});
-});
+//app.listen(3000);
 
-app.get('/about-us', (req,res) =>{
-    res.redirect('/about');
-});
+// app.use((req,res,next) => {
+//   console.log('New request made');
+//   console.log('host',req.hostname);
+//   console.log('path',req.path);
+//   console.log('method',req.method);
+//   next();
+// });
+// app.use((req,res,next) => {
+//     console.log('in the next middleware');
+//     next();
+// });
+
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true}));
+app.use(morgan('dev'));
+
+// app.get('/add-blogs', (req, res) => {
+//     const blog = new Blog({
+//         title: 'new blog',
+//         snippet: 'about my new blog',
+//         body: 'more about my new blog'
+//     });
+
+//     blog.save()
+//     .then((result) => app.listen(3000))
+//     .catch((err) => {
+//         console.log(err);
+//     });
+
+// });
+
+
+// app.get('/all-blogs', (req, res) => {
+//   Blog.find()
+//     .then((result) => {
+//         res.send(result);
+//     })
+//     .catch((err) =>{
+//         console.log(err);
+//     });
+// });
+
+// app.get('/single-blog',(req,res) =>{
+//     Blog.findById('')
+//     .then((result) => {
+//         res.send(result);
+//     })
+//     .catch((err) =>{
+//         console.log(err);
+//     });
+// })
+
+
+
+app.get('/blogs',async (req,res) => {
+//    Blog.find().sort({createdAt: -1})
+//    .then((result) => {
+//              res.json
+//           })
+//          .catch((err) =>{
+//              console.log(err);
+//          });
+
+         const blogs = await Blog.find();
+         res.send(blogs);
+})
+
+app.get("/blogs/:id", async(req, res)=> {
+    const blog = await Blog.findById(req.params.id);
+    res.send(blog)
+})
+
+app.post('/blogs',async(req,res) => {
+    const blog = new Blog(req.body);
+    const createdBlog = await blog.save();
+    res.send(createdBlog);
+})
+
+app.delete("/blogs/:id", async(req,res)=>{
+    const delBlog = await Blog.findByIdAndDelete(req.params.id);
+    res.send("deleted successfuly!")
+})
+
+app.put("/blogs/:id", async(req,res) =>{
+    const updateBlog = await Blog.findByIdAndUpdate(req.params.id,req.body, {new:true});
+    res.send(updateBlog)
+})
 
 app.use((req,res) => {
-    res.status(404).sendFile('./views/404.html', {root: __dirname})
+    res.status(404).render('404',{title: '404'});
 });
